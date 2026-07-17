ff3384d3d9d9b2838a468b481dc7fa53c10c3c6a
Arasbabacmd <erdemiraraseymen936@gmail.com>
2026-07-17 13:02:03 +0300

    Update rblxkopyala.js

--- commands/rblxkopyala.js
+++ commands/rblxkopyala.js
@@ -1,5 +1,4 @@
 const { SlashCommandBuilder, AttachmentBuilder, EmbedBuilder } = require('discord.js');
-const fetch = require('node-fetch');
 
 module.exports = {
     data: new SlashCommandBuilder()
